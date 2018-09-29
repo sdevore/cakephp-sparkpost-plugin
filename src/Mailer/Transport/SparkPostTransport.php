87d733a23b1330834e6bf87c2bdcbf7305c18190
Sam DeVore <sdevore@gmail.com>
2018-09-29 15:17:27 -0700

    update transport for multiple recipient arrays and new spark post classes

--- src/Mailer/Transport/SparkPostTransport.php
+++ src/Mailer/Transport/SparkPostTransport.php
@@ -15,15 +15,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 namespace SparkPost\Mailer\Transport;
 
 use Cake\Core\Configure;
 use Cake\Mailer\AbstractTransport;
 use Cake\Mailer\Email;
 use Cake\Network\Exception\BadRequestException;
-use Cake\Network\Http\Client;
-use Ivory\HttpAdapter\CakeHttpAdapter;
-use SparkPost\APIResponseException;
+use Cake\Http\Client as CakeClient;
+use Cake\Utility\Text;
+use Http\Adapter\Cake\Client as CakeAdaptor;
+use Migrations\CakeAdapter;
 use SparkPost\SparkPost;
 
 /**
@@ -35,45 +37,124 @@ use SparkPost\SparkPost;
  */
 class SparkPostTransport extends AbstractTransport
 {
+    protected $_receipients = []; // array of recipients per sparkpost formats
+    protected $_substitution_data = [];
+    protected $_description = NULL; //Description of the transmission. Maximum length - 1024 bytes
+    protected $_campaign_id = NULL; //Name of the campaign. Maximum length - 64 bytes
+
     /**
      * Send mail via SparkPost REST API
      *
      * @param \Cake\Mailer\Email $email Email message
      * @return array
      */
-    public function send(Email $email)
-    {
+    public function send(Email $email) {
         // Load SparkPost configuration settings
-        $apiKey = $this->config('apiKey');
+        $apiKey = $this->getConfig('ScidSparkPost.apiKey');
 
         // Set up HTTP request adapter
-        $adapter = new CakeHttpAdapter(new Client());
+        $adapter = new CakeAdaptor(new CakeClient());
 
         // Create SparkPost API accessor
-        $sparkpost = new SparkPost($adapter, [ 'key' => $apiKey ]);
+        $sparkpost = new SparkPost($adapter, ['key' => $apiKey]);
 
         // Pre-process CakePHP email object fields
-        $from = (array) $email->from();
-        $sender = sprintf('%s <%s>', array_values($from)[0], array_keys($from)[0]);
-        $to = (array) $email->to();
-        $recipients = [[ 'address' => [ 'name' => array_values($to)[0], 'email' => array_keys($to)[0] ]]];
+        $from = (array)$email->getFrom();
+        if (empty($this->_receipients)) {
+            $toArray = $email->getTo();
+            $recipients = [
+                ['address' => []],
+            ];
+        } else {
+            $recipients = $this->_receipients;
+        }
+        $message = [
+            'content' => [
+                'from'    => $from,
+                'subject' => $email->getSubject(),
+                'html'    => empty($email->message('html')) ? $email->message('text') : $email->message('html'),
+                'text'    => $email->message('text'),
+            ],
+        ];
+        $cc = $email->getCc();
+        $bcc = $email->getBcc();
+
 
         // Build message to send
-        $message = [
-            'from' => $sender,
-            'html' => empty($email->message('html')) ? $email->message('text') : $email->message('html'),
-            'text' => $email->message('text'),
-            'subject' => $email->subject(),
-            'recipients' => $recipients
-        ];
+        if (!empty($this->_description)) {
+            $message['description'] = $this->_description;
+        }
+        if (!empty($this->_campaign_id)) {
+            $message['campaign_id'] = $this->_campaign_id;
+        }
+        if (!empty($this->_substitution_data)) {
+            $message['substitution_data'] = $this->_substitution_data;
+        }
 
         // Send message
+        $promise = $sparkpost->transmissions->post($message);
         try {
-            $sparkpost->transmission->send($message);
-        } catch(APIResponseException $e) {
+            $response = $promise->wait();
+            $return = [
+                'result' => 'success',
+                'code'=>$response->getStatusCode(),
+                'body' => $response->getBody(),
+            ];
+            return $return;
+        } catch (\Exception $e) {
             // TODO: Determine if BRE is the best exception type
-            throw new BadRequestException(sprintf('SparkPost API error %d (%d): %s (%s)',
-                $e->getAPICode(), $e->getCode(), ucfirst($e->getAPIMessage()), $e->getAPIDescription()));
+            throw new BadRequestException(__('SparkPost API error {0}: {1}',
+                                             [$e->getCode(), ucfirst($e->getMessage())]));
         }
+    }
+
+    public function setDescription ($description) {
+        $this->_description = Text::truncate($description, 500 );
+    }
+
+    public function setCampaignId($id) {
+        $this->_campaign_id = Text::truncate($id, 60);
+    }
+
+    /**
+     * @param   string    $email
+     * @param null|string $name
+     * @param null|array  $subsitution_data
+     *
+     * @param null|array  $tags     Array of text labels associated with a recipient. Tags are available in Webhook events. Maximum number of tags - 10 per recipient,
+     *                              100 system wide. Any tags over the limits are ignored.
+     * @param null|array  $metadata Key/value pairs associated with a recipient. Metadata is available during events through the Webhooks and is provided to
+     *                              the template engine. A maximum of 1000 bytes of merged metadata (transmission level + recipient level) is available
+     *                              with recipient metadata taking
+     *                              precedence over transmission metadata when there are conflicts.
+     * @return $this
+     */
+    public function addRecipient($email, $name = NULL, $subsitution_data = NULL, $tags = NULL, $metadata = NULL) {
+        $recipient = ['address' => ['email']];
+        if (!empty($name)) {
+            $recipient['address']['name'] = h($name);
+        }
+        if (!empty($subsitution_data)) {
+            $recipient['substitution_data'] = $subsitution_data;
+
+        }
+        if (!empty($tags)) {
+            $recipient['tags'] = $tags;
+        }
+        if (!empty($metadata)) {
+            $recipient['metadata'] = $metadata;
+        }
+        $this->_receipients[] = $recipient;
+        return $this;
+    }
+
+    /**
+     * @param array $substitution_data
+     *
+     * @return $this
+     */
+    public function addSubtitutionData($substitution_data) {
+
+        return $this;
     }
 }
